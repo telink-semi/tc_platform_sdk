7759463feac56931afb9093e53e58355ba16678f
chenxi.wang <chenxi.wang@telink-semi.com>
2023-03-20 15:04:31 +0800

    feat(all):release v1.5.1

--- chip/B80/drivers/lib/include/rf_drv.h
+++ chip/B80/drivers/lib/include/rf_drv.h
@@ -930,7 +930,7 @@ static inline unsigned char is_rf_receiving_pkt(void)
  */
 extern void rf_set_channel_500k(signed short chn, unsigned short set);
 /**
-*	@brief		this function is to set shock burst for RF.
+*	@brief		This function is to set Private mode payload len for RF.
 *	@param[in]	len - length of payload.
 *	@return	 	none.
 */
